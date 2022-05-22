70025a7fbe191a177aa1e99a5626d14fb2d40b6f
Ade Gbodimowo <agbodimowo@erpinternational.com>
2022-05-22 18:53:31 -0400

    Create new app ami

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ data "aws_subnet" "jango" {
 
 
 output "aws_security_group" "jango" {
-  name = "${data.aws_security_group.jango.security_group_id}"
+  name = "${data.aws_security_group.jango.name}"
 }
 
 #module "instance" {
